217644fbac9f733e3a8d16d49813e5b881197c2b
KieranOlney <kieran.olney@outlook.com>
2022-04-21 13:32:23 +0100

    extension of initial commit

---
+++ animals.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from asyncio.windows_events import NULL
+
+class animal(ABC):
+    #Attributes
+    age = NULL
+    name = NULL
+    #Contructors
+    
+    #Methods
+
+
+class mammal(animal):
+    #Attributes
+
+    #Contructors
+
+    #Methods
+
+class cat(mammal):
+    #Attributes
+
+    #Contructors
+
+    #Methods    
