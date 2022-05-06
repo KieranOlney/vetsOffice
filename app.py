a93f07ee462456372cb201c0cb39c1b46dfe28af
KieranOlney <kieran.olney@outlook.com>
2022-05-06 12:27:23 +0100

    fixing all Major and Critical code smells. Renaming variables and functions to match convention (all lowercase) Switched from wildcard import to named functions

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from animalGenerator import generateobjects 
 from flask import Flask, request, jsonify
-from vetFuncs import *
+from vetFuncs import findOwnerById,findOwnerByName,findOwnerByPetId,findPetById,findPetByName,findPetsByOwnerId,readJSONFiles
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -11,45 +10,45 @@ def home():
     return "<h1>Welcome to Kieran's API work</h1><a href='/api/customers/all'>Customer List</a><p></p><a href='/api/pets/all'>Pet List</a>"
 
 @app.route("/api/customers", methods=["GET"])
-def getCustomerInfo():
+def getcustomerinfo():
     if 'ownerId' in request.args:
-        id = int(request.args['ownerId'])
-        ownerInfo = findOwnerById(id)
+        ownerid = int(request.args['ownerId'])
+        ownerinfo = findOwnerById(ownerid)
     elif 'petId' in request.args:
-        id = int(request.args['petId'])
-        ownerInfo = findOwnerByPetId(id)
+        ownerid = int(request.args['petId'])
+        ownerinfo = findOwnerByPetId(ownerid)
     elif 'ownerName' in request.args:
-        name = str(request.args['ownerName'])
-        ownerInfo = findOwnerByName(name)
+        ownername = str(request.args['ownerName'])
+        ownerinfo = findOwnerByName(ownername)
     else:
         return "Error: No Recognised Argument Provided, This function's Argument should be 'ownerId' or 'petId"
 
-    return jsonify(ownerInfo)
+    return jsonify(ownerinfo)
 
 @app.route("/api/customers/all", methods=["GET"])
 def getAllCustomers():
-    ownerInfo, petInfo = readJSONFiles()
-    return jsonify(ownerInfo)
+    ownerinfo, petinfo = readJSONFiles()
+    return jsonify(ownerinfo)
 
 @app.route("/api/pets", methods=["GET"])
-def getPetInfo():
+def getpetinfo():
     if 'petId' in request.args:
-        id = int(request.args['petId'])
-        petInfo = findPetById(id)
+        petid = int(request.args['petId'])
+        petinfo = findPetById(petid)
     elif 'ownerId' in request.args:
-        id = int(request.args['ownerId'])
-        petInfo = findPetsByOwnerId(id)
+        petid = int(request.args['ownerId'])
+        petinfo = findPetsByOwnerId(petid)
     elif 'petName' in request.args:
-        name = str(request.args['petName'])
-        petInfo = findPetByName(name)
+        petname = str(request.args['petName'])
+        petinfo = findPetByName(petname)
     else:
         return "Error: No Recognised Argument Provided, This function's Argument should be 'petId' or 'ownerId'"
-    return jsonify(petInfo)
+    return jsonify(petinfo)
 
 @app.route("/api/pets/all", methods=["GET"])
 def getAllPets():
-    ownerInfo, petInfo = readJSONFiles()
-    return jsonify(petInfo)
+    ownerinfo, petinfo = readJSONFiles()
+    return jsonify(petinfo)
 
 if __name__ == "__main__":
     app.run()
